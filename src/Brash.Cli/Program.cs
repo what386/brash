ccfa8f353eaea3a9b7c9e9003d76b901e80c5c2f
bmorin <robot0324@gmail.com>
2026-02-06 22:31:07 -0500

    rm custom version flag

--- src/Brash.Cli/Program.cs
+++ src/Brash.Cli/Program.cs
@@ -5,26 +5,12 @@ using Brash.Cli.Application;
 
 internal static class Program
 {
-    private const string Version = "0.1.0-beta";
-
     private static async Task<int> Main(string[] args)
     {
         var rootCommand = new RootCommand("Brash CLI");
 
-        var versionOption = new Option<bool>("--version")
-        {
-            Description = "Show Brash CLI version."
-        };
-
-        rootCommand.Options.Add(versionOption);
         rootCommand.SetAction(parseResult =>
         {
-            if (parseResult.GetValue(versionOption))
-            {
-                Console.WriteLine($"brash {Version}");
-                return 0;
-            }
-
             Console.WriteLine("Use `brash --help` to see available commands.");
             return 0;
         });
